608bae9104a1b308c07e9a6c157568622472be1e
woodychang0611 <woodychang0611@gmail.com>
2016-02-05 15:55:03 +0800

    03:54

--- woodychang0611_common/image.cpp
+++ woodychang0611_common/image.cpp
@@ -10,8 +10,4 @@
 	template void  WOODYCHANG0611_COMMON_API woodychang0611::image::ImageGeneric<woodychang0611::color::Gray8>::
 		CopyFrom(const  woodychang0611::image::ImageGeneric<woodychang0611::color::RGB24>& src);
 
-	//template class WOODYCHANG0611_COMMON_API  std::vector <woodychang0611::image::ImageRGB24>;
-	//template class WOODYCHANG0611_COMMON_API  std::vector <woodychang0611::image::ImageGray8>;
-	//template class WOODYCHANG0611_COMMON_API  std::vector <std::vector <woodychang0611::image::ImageRGB24> >;
-	//template class WOODYCHANG0611_COMMON_API  std::vector <std::vector <woodychang0611::image::ImageGray8> >;
 #endif
